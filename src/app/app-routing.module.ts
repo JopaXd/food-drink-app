b1a149b712f1db85703e67b264682d01adba4b1a
JopaXd <pavle_milanov@hotmail.com>
2020-10-03 20:51:28 +0200

    Added basic look and components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { CategoriesComponent } from './categories/categories.component'
+import { AreaComponent } from './area/area.component'
+import { IngredientsComponent } from './ingredients/ingredients.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent},
+	{ path: 'categories', component: CategoriesComponent },
+	{ path: 'area', component: AreaComponent },
+	{ path: 'ingredients', component: IngredientsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AreaComponent } from './area/area.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CategoriesComponent,
+    AreaComponent,
+    IngredientsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
