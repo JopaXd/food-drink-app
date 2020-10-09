0f1b3d5d9476797a0e1b747097df9eaee7a8076f
JopaXd <pavle_milanov@hotmail.com>
2020-10-09 18:58:22 +0200

    Added frontend for browsing by area.

---
+++ src/app/area.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AreaService {
+
+  constructor(private http:HttpClient) { }
+
+  getAllAreas(){
+  	return this.http.get("https://www.themealdb.com/api/json/v1/1/list.php?a=list");
+  }
+}
